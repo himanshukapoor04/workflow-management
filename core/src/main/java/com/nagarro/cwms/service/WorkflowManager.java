d5f26d7f63f858801497bb04d8e79f6781c9035d
Himanshu Kapoor <himanshu.kapoor.ext@netent.com>
2016-06-29 17:07:51 +0200

    Adding files

---
+++ core/src/main/java/com/nagarro/cwms/service/WorkflowManager.java
@@ -0,0 +1,6 @@
+package com.nagarro.cwms.service;
+
+
+public class WorkflowManager {
+
+}
--- webadmin/src/main/java/com/nagarro/cwms/webadmin/web/controller/WorkflowInstanceController.java
+++ webadmin/src/main/java/com/nagarro/cwms/webadmin/web/controller/WorkflowInstanceController.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value="/workflow")
 public class WorkflowInstanceController {
 
-	@RequestMapping(value="/allWorkflows")
+	@RequestMapping(value="/allWorkflows",method = RequestMethod.GET)
 	public ModelAndView showWorkflows() {
 		ModelAndView model = new ModelAndView();
-		model.setViewName("showWorkflows");
+		model.setViewName("showWorkFlow");
 		model.addObject("workflows",new ArrayList<Object>());
 		return model;
 	}
